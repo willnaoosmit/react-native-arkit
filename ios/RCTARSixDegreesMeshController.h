aff2983025dfb94cae75ecec349cfa172aa418cf
H. Cole Wiley <cole@scandy.co>
2019-01-16 16:28:39 -0600

    moved SixDegreesMeshController to separate file

---
+++ ios/RCTARSixDegreesMeshController.h
@@ -0,0 +1,17 @@
+//
+//  RCTARSixDegreesMeshController.h
+//  RCTARKit
+//
+//  Created by H. Cole Wiley on 1/16/19.
+//  Copyright © 2019 HippoAR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTARSixDegreesMeshController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RCTARSixDegreesMeshController.m
@@ -0,0 +1,13 @@
+//
+//  RCTARSixDegreesMeshController.m
+//  RCTARKit
+//
+//  Created by H. Cole Wiley on 1/16/19.
+//  Copyright © 2019 HippoAR. All rights reserved.
+//
+
+#import "RCTARSixDegreesMeshController.h"
+
+@implementation RCTARSixDegreesMeshController
+
+@end
